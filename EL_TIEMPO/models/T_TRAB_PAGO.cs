cb88e8df8829e9a8ed8c063f7a99dc6f934c644d
Yestli06 <yestlisjmeneses@gmail.com>
2022-08-24 16:52:23 -0500

    Se agregaron las foraneas en T_PRO_BOD y T_TRAB_PAGO

--- EL_TIEMPO/models/T_TRAB_PAGO.cs
+++ EL_TIEMPO/models/T_TRAB_PAGO.cs
@@ -7,11 +7,19 @@ namespace MI_TERRAZA.Models
             [Key]
             [Required(ErrorMessage = "Este campo es obligatorio")]
             public int TRABPAGO_ID { get; set; }
+
             [Required(ErrorMessage = "Este campo es obligatorio")]
             [Display(Name ="Id del trabajador")]
+            [ForeignKey("T_TRABAJADOR")]
             public int TRABPAGO_TRAB_ID { get; set; }
+
+            public T_TRABAJADOR? T_TRABAJADOR  { get; set; }
+
             [Display(Name ="Id del pago")]
+            [ForeignKey("T_PAGO")]
             public int TRABPAGO_PAGO_ID { get; set; }
+
+             public T_PAGO? T_PAGO  { get; set; }
         
     }
 }
