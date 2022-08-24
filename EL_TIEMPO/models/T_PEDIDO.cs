059d80b1064d277fd0b98594a23521cc5512e60f
SAMUEL DAVID RODRIGUEZ ROMERO <Samuel2273Davidq@outlook.es>
2022-08-24 16:47:53 -0500

    Llave foranea  a las tablas

--- EL_TIEMPO/models/T_PEDIDO.cs
+++ EL_TIEMPO/models/T_PEDIDO.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MI_TERRAZA.Models
 {
+    [Table("T_PEDIDO")]
     public class T_PEDIDO
     {
         public int PED_ID { get; set; }
@@ -10,7 +12,11 @@ namespace MI_TERRAZA.Models
         public string? PED_ESTADO { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
         [Display(Name = "ID Del Cliente :")]
+        [ForeignKey("T_CLIENTE")]
         public int PED_CLI_ID { get; set; }
+
+        public T_CLIENTE? T_CLIENTE { get; set; }
+
         [Required(ErrorMessage = "Este campo es obligatorio")]
         [Display(Name = "Fecha Del Pedido :")]
         public DateTime PED_FECHA { get; set; }
--- EL_TIEMPO/models/T_TRAB_PRES.cs
+++ EL_TIEMPO/models/T_TRAB_PRES.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MI_TERRAZA.Models
 {
+        [Table("T_TRAB_PRES")]
     public class T_TRAB_PRES
     {
         [Key]
@@ -9,10 +11,18 @@ namespace MI_TERRAZA.Models
         public int TRABPRES_ID { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
         [Display(Name ="Id del trabajador")]
+        [ForeignKey("T_TRABAJADOR")]
         public int TRABPRES_TRAB_ID { get; set; }
+
+        public T_TRABAJADOR? T_TRABAJADOR { get; set; }
+
         [Required(ErrorMessage = "Este campo es obligatorio")]
         [Display(Name ="Id del prestamo")]
+        [ForeignKey("T_PRESTAMO")]
         public int TRABPRES_PRES_ID { get; set; }
+
+        public T_PRESTAMO? T_PRESTAMO { get; set; }
+
 
     }
 }
