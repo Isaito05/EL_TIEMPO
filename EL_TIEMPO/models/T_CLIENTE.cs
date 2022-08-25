d89da2b994165897b8032d44dfbe2197f6a99c2d
YeiderCabeza <ycabeza5@misena.edu.co>
2022-08-24 21:27:08 -0500

    Llaves foraneas

--- EL_TIEMPO/models/T_CLIENTE.cs
+++ EL_TIEMPO/models/T_CLIENTE.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MI_TERRAZA.Models
 {
@@ -9,6 +10,9 @@ namespace MI_TERRAZA.Models
         public int CLI_ID { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
         [Display(Name ="Identificacion Del Usuario :")]
+        
+        [ForeignKey("T_RGU_USUARIO")]
         public int CLI_RGU_IDENTIFICACION { get; set; }
+        public T_RGU_USUARIO? T_RGU_USUARIO { get; set; }
     }
 }
