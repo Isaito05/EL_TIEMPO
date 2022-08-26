8d14663ac11ac35bd41d5fbe14d0d7ad9b285382
isaito05 <deleondiazisaacdavid@gmail.com>
2022-08-26 10:03:43 -0500

    Llaves foraneas

--- EL_TIEMPO/models/T_BODEGA.cs
+++ EL_TIEMPO/models/T_BODEGA.cs
@@ -6,7 +6,7 @@ namespace MI_TERRAZA.Models
     {
         [Key]
         public int BOD_ID { get; set; }
-        [Required(ErrorMessage = "Este campo es Obligatorio")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public int BOD_STOCK_MINIMO { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
         public string? BOD_ESTADO { get; set; }
